738995de21caee5e174a5dab37bf220c67213d8c
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-01-28 22:43:08 +0600

    Programming hero practice project javaScript break (Module No-15)

---
+++ break.js
@@ -0,0 +1,22 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+let a = 1
+while (a <= 200) {
+    console.log(a)
+    if (a === 100) {
+        break
+    }
+    a++
+}
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
